86e4a0464f99f62dc707956da14593f2823493eb
danielKatsarov <116427485+danielKatsarov@users.noreply.github.com>
2025-10-22 16:28:09 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import statistics
+
+grades = [5, 6, 4, 6, 3, 5, 4, 6]
+
+avg = statistics.mean(grades)
+
+med = statistics.median(grades)
+
+mode = statistics.mode(grades)
+
+print("Средна стойност:", avg)
+print("Медиана:", med)
+print("Мода:", mode)
+
+lowest = min(grades)
+highest = max(grades)
+
+print("Най-висока оценка:", highest)
+print("Най-ниска оценка:", lowest)
+
+count_above_avg = sum(1 for grade in grades if grade > avg)
+print("Брой ученици с успех над средния:", count_above_avg)
+
+if (avg >= 5):
+    print("Отличен")
+elif (avg >= 4):
+    print("Много добър")
+else:
+    print("Трябва повече старание")
